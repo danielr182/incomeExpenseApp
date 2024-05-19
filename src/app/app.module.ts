7d2dc01da58e62f248e19456041228627acd8d16
Daniel Rodriguez M <daniel.rodriguez182@gmail.com>
2024-05-18 23:02:02 -0500

    Upgraded ngrx to v17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ import { AuthModule } from './auth/auth.module';
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
-    }),
+    , connectInZone: true}),
   ],
   providers: [],
   bootstrap: [AppComponent],
